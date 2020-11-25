0c5a6c6d20f59cc73aa286ecc323d4474f75975e
jvbelandres <jvbelandres@github.com>
2020-11-25 10:39:50 +0800

    Created the functionlity of User Registration

--- InterviewerBot/user/views.py
+++ InterviewerBot/user/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View, TemplateView
-# from django.http import Http404
-# from django.http import HttpResponse
-# from .forms import *
-# from .models import *
+from django.http import Http404
+from django.http import HttpResponse
+from .forms import *
+from .models import *
 from django.core.files.storage import default_storage
 # Create your views here.
 
@@ -30,6 +30,25 @@ class JobInterviewView(View):
 class UserRegistrationView(View):
 	def get(self, request):
 		return render(request, 'RegistrationPage.html')
+
+	def post(self, request):
+		form = ApplicantForm(request.POST)
+
+		if(form.is_valid()):
+			fname = request.POST.get("firstname")
+			lname = request.POST.get("lastname")
+			password = request.POST.get("password")
+			gender = request.POST.get("gender")
+			emailAdd = request.POST.get("emailAddress")
+
+			form = Applicant(firstname = fname, lastname = lname, password = password, gender = gender, 
+								emailAddress = emailAdd)
+			form.save()
+
+			return redirect('user:login_view')
+		else:
+			print(form.errors)
+			return HttpResponse('not valid')
 
 class JobOffersView(View):
 	def get(self, request):
