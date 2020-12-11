9a9fb47318dcc3b85af29a8c5f3d3e0d10f9e39e
jvbelandres <jvbelandres@github.com>
2020-12-12 01:49:33 +0800

    Added dialog box sa user-side and Required password format
    Note: Ako lang sa gipang comment ang para pagination sa job-offers.
    
    Nakasuway kog gana sa button SEARCH
    (ga experiment2 ko) pero kailangan complete job title jud ang i-input.

--- InterviewerBot/user/views.py
+++ InterviewerBot/user/views.py
@@ -1,3 +1,5 @@
+import ctypes
+from tkinter import messagebox as tkMessageBox
 from django.shortcuts import render, redirect
 from django.views.generic import View, TemplateView
 from django.http import Http404
@@ -11,6 +13,40 @@ from django.core.mail import send_mail
 from django.core.paginator import Paginator
 
 # Create your views here.
+
+def Mbox(title, text, style):
+	return ctypes.windll.user32.MessageBoxW(0, title, text, style)
+
+def password_check(password):
+	specialSymbols = ['$', '*', '#', '@', '%', '!', '^', '&']
+	val = True
+
+	if len(password) < 6: 
+		Mbox('length should be at least 6', 'Error', 16) 
+		val = False
+
+	if len(password) > 20:
+		Mbox('length should be not be greater than 8', 'Error', 16) 
+		val = False  
+
+	if not any(char.isdigit() for char in password): 
+		Mbox('Password should have at least one numeral', 'Error', 16) 
+		val = False
+
+	if not any(char.isupper() for char in password): 
+		Mbox('Password should have at least one uppercase letter', 'Error', 16) 
+		val = False
+
+	if not any(char.islower() for char in password): 
+		Mbox('Password should have at least one lowercase letter', 'Error', 16) 
+		val = False
+
+	if not any(char in specialSymbols for char in password): 
+		Mbox('Password should have at least one of the symbols $@#*@%!^&', 'Error', 16) 
+		val = False
+
+	if val:
+		return val
 
 class UserIndexView(View):
 	def get(self, request):
@@ -31,6 +67,7 @@ class UserIndexView(View):
 					form.emailAddress = email
 					form.password = password
 					form.save()
+					Mbox('WELCOME '+applicant.firstname, 'Success', 64)
 				return redirect('user:home_view')
 
 		for administrator in administrators:
@@ -41,9 +78,11 @@ class UserIndexView(View):
 					form.emailAddress = email
 					form.password = password
 					form.save()
+					Mbox('WELCOME '+administrator.firstname, 'Success', 64)
 				return redirect('administrator:dashboard_view')
 
-		return HttpResponse('Email address or password is incorrect.')
+		Mbox('Email address or password is inccorect', 'Error', 16)
+		return redirect('user:login_view')
 
 class AboutUsView(View):
 	def get(self, request):
@@ -127,8 +166,10 @@ class SettingsView(View):
 		phone = request.POST.get("phone")
 		password = request.POST.get("password")
 
-		update_applicant = Applicant.objects.filter(id = applicant_id).update(firstname = firstName,
-			lastname = lastName, phone = phone, password = password)
+		if password_check(password):
+			update_applicant = Applicant.objects.filter(id = applicant_id).update(firstname = firstName,
+				lastname = lastName, phone = phone, password = password)
+			Mbox('Profile Update Successful', 'Success', 64)
 
 		return redirect('user:settings_view')
 
@@ -154,17 +195,20 @@ class UserRegistrationView(View):
 				password = request.POST.get("pass")
 				gender = request.POST.get("gender")
 				emailAdd = request.POST.get("email")
+				
+				if password_check(password):
+					form = Applicant(firstname = fname, lastname = lname, phone = phone, password = password, gender = gender, 
+											emailAddress = emailAdd)
+					form.save()
+					Mbox('Successfully Registered', 'Success', 64)
 
-				form = Applicant(firstname = fname, lastname = lname, phone = phone, password = password, gender = gender, 
-										emailAddress = emailAdd)
-				form.save()
-
-				return redirect('user:login_view')
+					return redirect('user:login_view')
 		else:
 			print(form.errors)
 			return HttpResponse('Email address is already used.')
 		
-		return HttpResponse('not valid')
+		Mbox('Registration failed', 'Error', 16) 
+		return redirect('user:registration_view')
 
 class JobOffersView(View):
 	def get(self, request):
@@ -172,20 +216,21 @@ class JobOffersView(View):
 		applicant = Applicant.objects.filter(id = currentUser)
 		joblists = CreateJob.objects.raw('SELECT createjob.id, createjob.title, createjob.description FROM createjob WHERE createjob.id NOT IN (SELECT savedjobs.job_id FROM savedjobs, currentuser WHERE currentuser.user_id = savedjobs.user_id)')
 
-		p = Paginator(joblists,3)
-		page_number = request.GET.get('page',1)
-		page = p.page(page_number)
-		numberOfPage = p.num_pages
+		# p = Paginator(joblists,3)
+		# page_number = request.GET.get('page',1)
+		# page = p.page(page_number)
+		# numberOfPage = p.num_pages
 
-		array = []
-		for x in range(1, numberOfPage+1):
-			array.append(x)
+		# array = []
+		# for x in range(1, numberOfPage+1):
+		# 	array.append(x)
 
 		context = {
 			'applicant': applicant,
-			'joblists': page,
-			'pages': array,
-			'page_number': int(page_number)
+			'joblists': joblists,
+			# 'joblists': page,
+			# 'pages': array,
+			# 'page_number': int(page_number)
 		}
 
 		return render(request, 'jobOffers.html', context)
@@ -207,6 +252,17 @@ class JobOffersView(View):
 				if count < 5:
 					save_jobs = SavedJobs.objects.create(job_id = job_id, user_id = user_id, job_header = job_title, job_description = job_description)
 					return redirect('user:job-offers_view')
+			# elif 'btnSearch' in request.POST:
+			# 	searchData = request.POST.get("search")
+			# 	searchData = searchData.split()
+			# 	print(searchData)
+			# 	joblists = CreateJob.objects.raw('SELECT createjob.id, createjob.title, createjob.description FROM createjob WHERE createjob.title = %s AND createjob.id NOT IN (SELECT savedjobs.job_id FROM savedjobs, currentuser WHERE currentuser.user_id = savedjobs.user_id)', [searchData])
+
+			# 	context = {
+			# 		'joblists': joblists,
+			# 	}
+
+			# 	return render(request, 'jobOffers.html', context)
 
 		return HttpResponse('You can only save at most 5 job offerings.')
 
